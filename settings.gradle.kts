c0c8b32aae0b94a5bb51b4906ff0e21ad7b46a67
AdonayMejia <adonay.mejia021@gmail.com>
2023-06-14 14:01:26 -0600

    feat: Creating Project

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+import org.gradle.kotlin.dsl.provider.inClassPathMode
+
+include(":data")
+
+
+include(":domain")
+
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
+rootProject.name = "MusicPlayerCompose"
+include(":app")
