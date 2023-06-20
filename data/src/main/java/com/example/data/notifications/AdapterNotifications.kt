8c81590de8c88b23637e358f9ddd46da2cc73161
AdonayMejia <adonay.mejia021@gmail.com>
2023-06-20 15:14:45 -0600

    feat: Creating notification channel

--- data/src/main/java/com/example/data/notifications/AdapterNotifications.kt
+++ data/src/main/java/com/example/data/notifications/AdapterNotifications.kt
@@ -1,8 +1,11 @@
 package com.example.data.notifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationManagerCompat
 import androidx.media3.exoplayer.ExoPlayer
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -12,5 +15,24 @@ class AdapterNotifications @Inject constructor(
     @ApplicationContext private val context: Context,
     private val exoPlayer: ExoPlayer
 ) {
-    
+    private var notificationManager: NotificationManagerCompat = NotificationManagerCompat.from(context)
+
+    init {
+        createsNotificationChannel()
+    }
+
+    private fun createsNotificationChannel(){
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object{
+        private const val NOTIFICATION_ID = 169
+        private const val CHANNEL_NAME = "channel 1"
+        private const val CHANNEL_ID = "channel id 1"
+    }
 }
